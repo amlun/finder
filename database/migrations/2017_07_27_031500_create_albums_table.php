e353840c02fecd365b4a907b745685be2a98aca7
lunweiwei <lunweiwei@gmail.com>
2017-08-01 18:57:10 +0800

    fix table name

--- database/migrations/2017_07_27_031500_create_albums_table.php
+++ database/migrations/2017_07_27_031500_create_albums_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MakeAlbumsTable extends Migration
+class CreateAlbumsTable extends Migration
 {
     /**
      * Run the migrations.

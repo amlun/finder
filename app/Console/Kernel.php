99d8c9ca1d7731de0fc0565858c47ea36dd8865e
lunweiwei <lunweiwei@gmail.com>
2017-08-09 16:41:15 +0800

    修改cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,15 +34,13 @@ class Kernel extends ConsoleKernel
         $schedule->command('crawl:douban group 510760')->everyThirtyMinutes();
 
 
-//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/haixiuzu/"')->everyFiveMinutes();
-//
-//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/meituikong/"')->hourlyAt(5);
-//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/bw0766/"')->hourlyAt(5);
 //        $schedule->command('crawl:douban group 63686')->hourlyAt(10);
-//        $schedule->command('crawl:douban group 510760')->hourlyAt(10);
 //        $schedule->command('crawl:douban group 561425')->hourlyAt(15);
 //        $schedule->command('crawl:douban group 481977')->hourlyAt(20);
 //        $schedule->command('crawl:douban group 515085')->hourlyAt(20);
+
+//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/bw0766/"')->hourlyAt(5);
+//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/meituikong/"')->hourlyAt(5);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/368701/"')->hourlyAt(25);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/196602/"')->hourlyAt(25);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/531651/"')->hourlyAt(30);
@@ -50,7 +48,7 @@ class Kernel extends ConsoleKernel
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/kaopulove/"')->hourlyAt(35);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/407518/"')->hourlyAt(35);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/503950/"')->hourlyAt(40);
-////        $schedule->command('crawl:douban group --url="https://www.douban.com/group/446091/"')->hourlyAt(40);
+//        $schedule->command('crawl:douban group --url="https://www.douban.com/group/446091/"')->hourlyAt(40);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/nanpengyou/"')->hourlyAt(45);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/542175/"')->hourlyAt(45);
 //        $schedule->command('crawl:douban group --url="https://www.douban.com/group/515085/"')->hourlyAt(50);
